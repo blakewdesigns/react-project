ffb4989f4eb676aeced4ceb69fe06a86d076d30e
Blake Walker <blake.wdesigns@gmail.com>
2019-05-03 10:54:49 -0400

    Updated JSX to pull data from an array.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,24 @@ import React from 'react';
 import './App.css';
 import img from './images/list.png'
 
+let todos = [
+{
+  id: 1,
+  description: "your description",
+  completed: false
+},
+{
+  id: 2,
+  description: "your description",
+  completed: false
+},
+{
+  id: 3,
+  description: "your description",
+  completed: false
+}
+]
+
 function App() {
   return (
     <div className="App">
@@ -9,17 +27,20 @@ function App() {
       <div id="container">
         <h1 id="listHeader">ToDo List</h1>
         <div id="divider">
-          <div className="list">
-            <ul className="listItems">
-              <input type="checkbox"></input><li>List example 1</li>
-            </ul>
-            <ul className="listItems">
-              <input type="checkbox"></input><li>List example 2</li>
-            </ul>
-            <ul className="listItems">
-              <input type="checkbox"></input><input type="text" id="textInput" placeholder="Add item"></input>
-            </ul>
-          </div>
+        </div>
+        <div className="list">
+          <ul className="listItems">
+            {todos.map(todo => (
+              <li key={todos.id} id="item">
+                <input type="checkbox" id="checks" defaultChecked={todo.completed}/>
+                {todo.description}
+            </li>
+            ))
+            }
+          </ul>
+          <ul className="listItems">
+            <input type="text" id="textInput" placeholder="Add item"></input>
+          </ul>
         </div>
       </div>
     </div>
