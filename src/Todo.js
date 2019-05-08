cdd68117e1b046edb8c44d1e9467dd65cea85a8a
Blake Walker <blakewdesigns@gmail.com>
2019-05-08 11:11:22 -0400

    Updated CSS

--- src/Todo.js
+++ src/Todo.js
@@ -3,10 +3,17 @@ import React from 'react'
 export default class Todo extends React.Component {
     render() {
         return (
-            <li id="item">
+            <li style={{listStyle}}>
                 <input type="checkbox" id="checks" defaultChecked={this.props.todo.completed}/>
                 {this.props.todo.description}
             </li>
         )
     }
+}
+
+const listStyle = {
+        padding: '8px 0px 0px 50px',
+        marginLeft: '10px',
+        textAlign: 'left'
+      
 }
